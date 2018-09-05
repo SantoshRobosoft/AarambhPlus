639de112c8479bb4f597f9f1d81323c0b374ccf3
SantoshKS <santosh.sahoo@robosoftin.com>
2018-09-05 12:43:35 +0530

    - pod files added

---
+++ AarambhPlus/AarambhPlus/Classes/Utilities/App Setups/APNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  APNavigationController.swift
+//  AarambhPlus
+//
+//  Created by Santosh Kumar Sahoo on 9/5/18.
+//  Copyright © 2018 Santosh Dev. All rights reserved.
+//
+
+import UIKit
+
+class APNavigationController: UINavigationController {
+
+}
