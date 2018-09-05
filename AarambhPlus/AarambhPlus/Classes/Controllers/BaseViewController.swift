51814bf4a628958d966ab070010855aa3b12371a
SantoshKS <santosh.sahoo@robosoftin.com>
2018-09-05 12:30:27 +0530

    Initial Setup

---
+++ AarambhPlus/AarambhPlus/Classes/Controllers/BaseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AarambhPlus
+//
+//  Created by Santosh Kumar Sahoo on 9/5/18.
+//  Copyright © 2018 Santosh Dev. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
