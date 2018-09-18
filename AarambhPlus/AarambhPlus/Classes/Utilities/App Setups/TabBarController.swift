23ffcd81f7853cdb8ae2d98d7250ebfa3b478915
jaganath pattanayak <>
2018-09-18 15:26:08 +0530

    -tab bar gradient Color

--- AarambhPlus/AarambhPlus/Classes/Utilities/App Setups/TabBarController.swift
+++ AarambhPlus/AarambhPlus/Classes/Utilities/App Setups/TabBarController.swift
@@ -13,10 +13,20 @@ enum TabBarItemType {
 }
 
 class TabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
+        let layerGradient = CAGradientLayer()
         super.viewDidLoad()
+        let colorTop = UIColor.colorRGB(255, g: 200, b: 55).cgColor
+        let colorBottom = UIColor.colorRGB(255, g: 128, b: 8).cgColor
+        layerGradient.colors = [colorTop, colorBottom]
+        layerGradient.locations = [0.0, 1.0]
+        
+        layerGradient.startPoint = CGPoint(x: 0, y: 0.5)
+        layerGradient.endPoint = CGPoint(x: 1, y: 0.5)
+        
+        layerGradient.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        self.tabBar.layer.addSublayer(layerGradient)
     }
-
     
 }
